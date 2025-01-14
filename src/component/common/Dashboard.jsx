161f19603f0c50c90b61fcbfd124b138c63f1cb5
jungyoonn <sophia7625@naver.com>
2025-01-14 18:17:26 +0900

    인덱스 페이지

---
+++ src/component/common/Dashboard.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { useAuth } from '../../hooks/AuthContext';
+
+const Dashboard = () => {
+  const {email, token, logout} = useAuth();
+
+  // const [email, setEmail] = useState(localStorage.getItem('email'));
+  // const [token, setToken] = useState(localStorage.getItem('token'));
+  // const navigate = useNavigate();
+
+  // const handleLogout = (e) => {
+  //   // setItem, getItem, removeItem
+  //   if(email) {
+  //     setEmail(localStorage.removeItem('email'));
+  //     setToken(localStorage.removeItem('token'));
+  //   } else {
+  //     navigate('/');
+  //   }
+  // }
+
+  return (
+    <div>
+      <h1>시작 페이지</h1>
+      <p>{email || 'guest'}</p>
+      <p>{token}</p>
+      {email ? <> <button onClick={logout}>로그아웃</button><Link to={"/list"}>글 목록</Link></> : <Link to={"/"}>로그인</Link>}
+      {/* {email && <Link to={'/list'}>글 목록</Link>} */}
+    </div>
+  );
+}
+
+export default Dashboard;
