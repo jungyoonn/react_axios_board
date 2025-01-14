5dcf28ab72a25ee35097c2ebdf08b3f36b088113
jungyoonn <sophia7625@naver.com>
2025-01-14 11:42:48 +0900

    토큰 추가

--- src/hooks/useAxios.jsx
+++ src/hooks/useAxios.jsx
@@ -1,12 +1,14 @@
 import { useCallback, useState } from 'react';
 import axios from 'axios';
 
-const BASE_URL = 'http://localhost:8080/api/v1/board/';
+const BASE_URL = 'http://localhost:8080/api/v1/';
 
 const UseAxios = (baseUrl = BASE_URL) => {
   const [data, setData] = useState(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
+
+  const tmpToken = 'eyJhbGciOiJIUzM4NCJ9.eyJpYXQiOjE3MzY4MTczMTEsImV4cCI6MTczOTQ5NTcxMSwic3ViIjoidXNlcjEwMEBlZWVycm9yY29kZS5jb20ifQ.cgMCIk7l_Wt-tSViqsfix5ETJF4-UYDdG8Z0-i1h2-WxSHHxRcO9cNRNGiC66eie';
 
   const req = useCallback(
     async (method, endpoint, body = null, addHeaders = {}) => {
@@ -19,6 +21,7 @@ const UseAxios = (baseUrl = BASE_URL) => {
           data: body,
           headers: {
             'Content-Type':'application/json',
+            'Authorization':`Bearer ${tmpToken}`,
             ...addHeaders
           }
         });
