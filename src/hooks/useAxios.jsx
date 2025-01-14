b0b5a155a112e2a544dc9ee1a73e2f304d64dfd6
jungyoonn <sophia7625@naver.com>
2025-01-14 18:16:32 +0900

    토큰 추가

--- src/hooks/useAxios.jsx
+++ src/hooks/useAxios.jsx
@@ -1,14 +1,14 @@
 import { useCallback, useState } from 'react';
 import axios from 'axios';
+import { useAuth } from './AuthContext';
 
 const BASE_URL = 'http://localhost:8080/api/v1/';
 
 const UseAxios = (baseUrl = BASE_URL) => {
   const [data, setData] = useState(null);
-  const [loading, setLoading] = useState(true);
+  const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
-
-  const tmpToken = 'eyJhbGciOiJIUzM4NCJ9.eyJpYXQiOjE3MzY4MTczMTEsImV4cCI6MTczOTQ5NTcxMSwic3ViIjoidXNlcjEwMEBlZWVycm9yY29kZS5jb20ifQ.cgMCIk7l_Wt-tSViqsfix5ETJF4-UYDdG8Z0-i1h2-WxSHHxRcO9cNRNGiC66eie';
+  const {token} = useAuth();
 
   const req = useCallback(
     async (method, endpoint, body = null, addHeaders = {}) => {
@@ -21,18 +21,19 @@ const UseAxios = (baseUrl = BASE_URL) => {
           data: body,
           headers: {
             'Content-Type':'application/json',
-            'Authorization':`Bearer ${tmpToken}`,
+            'Authorization':`Bearer ${token}`,
             ...addHeaders
           }
         });
         setData(resp.data);
+        return resp.data; // 이펙트를 쓰지 않아서 리턴 추가
       } catch(err) {
         setError(err);
       } finally {
         setLoading(false);
       }
     }
-  , [baseUrl]);
+  , [baseUrl, token]);
 
   return {data, loading, error, req};
 }
