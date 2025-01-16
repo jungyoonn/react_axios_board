ded6c8e8b1c3d61d7e15c1ad6d107743b62379c4
jungyoonn <sophia7625@naver.com>
2025-01-16 18:22:35 +0900

    첨부파일 포함 수정 폼

--- src/component/board/Modify.jsx
+++ src/component/board/Modify.jsx
@@ -1,11 +1,11 @@
 import React, { useEffect, useState } from 'react';
 import { Link, useNavigate, useParams } from 'react-router-dom';
 import UseAxios from '../../hooks/useAxios';
-import { useAuth } from '../../hooks/AuthContext';
 
 const Modify = () => {
-  const {email} = useAuth();
-  const [board, setBoard] = useState({title: '', content: '', memberEmail: email});
+  // const {email} = useAuth();
+  const [board, setBoard] = useState({title: '', content: '', memberEmail: '', attachDtos: []});
+  const [uploaded, setUploaded] = useState([]);
   const navigate = useNavigate();
   const {req} = UseAxios();
   const param = useParams();
@@ -16,6 +16,7 @@ const Modify = () => {
     (async () => {
       const resp = await req('get', `notes/${num}`);
       setBoard(resp);
+      setUploaded(resp.attachDtos);
     })();
   }, [req, num]);
 
@@ -29,11 +30,35 @@ const Modify = () => {
     console.log(board);
 
     // 빈 값에 대한 추가 처리 필요
-    req('put', `notes/${num}`, board);
+    req('put', `notes/${num}`, {...board, attachDtos: uploaded});
 
     alert("수정했습니다");
     navigate("/notes");
   }
+
+  
+  const handleFileUpload = async (e) => {
+    const files = e.target.files;
+
+    if(!files) {
+      return;
+    }
+
+    const formData = new FormData();
+    for(let i = 0; i < files.length; i++) {
+      formData.append("file", files[i]);
+    }
+  
+    try {
+      const result = await req('post', 'file/upload', formData, {'Content-Type':'multipart/form-data'});
+      console.log(result);
+      setUploaded([...uploaded, ...result]); 
+    } catch (error) {
+      console.error("Error during upload:", error);
+    }
+
+    e.target.value = '';
+  };
 
   return (
     <div>
@@ -45,6 +70,23 @@ const Modify = () => {
         <label>내용 : <textarea rows='5' name='content' id='content' value={board.content} onChange={handleChange} /></label>
         <br />
         <label>작성자 : <input type='text' name='memberEmail' id='memberEmail' value={board.memberEmail} onChange={handleChange} readOnly /></label>
+
+        <br />
+        <p>파일 추가 첨부 : </p>
+        <input type='file' onChange={handleFileUpload} name='file' multiple />
+        <br />
+
+        <ul>
+          {uploaded.map(u => <li key={u.uuid}><Link to={u.url}>{u.origin}</Link><button data-uuid={u.uuid} onClick={e => setUploaded(uploaded.filter(file => file.uuid !== e.currentTarget.dataset.uuid))}>삭제</button></li>)}
+        </ul>
+        <hr />
+        <div>
+          <h3>첨부파일 : {board.attachDtos.length}개</h3>
+          <ul>
+            {board.attachDtos.map(a => <li key={a.uuid}><Link to={a.url}>{a.origin}</Link></li>)}
+          </ul>
+        </div>
+
         <button>등록</button>
         <Link to={"/notes"}>목록</Link>
       </form>
